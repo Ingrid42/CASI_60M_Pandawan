59331eb04e75a102b161bb2f52f1a70080cf9137
IngridF <ingrid.fiquet31@gmail.com>
2017-12-15 14:22:06 +0100

    pac4j add pom.xml

---
+++ code/src/main/java/pandawan/entities/MyClientsFactory.java
@@ -0,0 +1,12 @@
+//package pandawan.entities;
+//
+//import org.pac4j.core.client.Clients;
+//
+//public class MyClientsFactory implements ClientsFactory {
+//    @Override
+//    public Clients build() {
+////        final FacebookClient facebookClient = new FacebookClient("fbKey", "fbSecret");
+//        	TODO : avec linkeded à la plac de FB ??
+//        return new Clients("http://localhost:8080/callback", facebookClient, twitterClient, googleClient);
+//    }
+//}
